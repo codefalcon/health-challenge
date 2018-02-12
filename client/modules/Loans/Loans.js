f751fbd83836c5c062c5a2dd6be94a71e663b1fd
Paul Pagnan <paul@pagnan.com.au>
2018-02-12 17:10:54 +1100

    Add phancy graphs.

--- client/modules/Loans/Loans.js
+++ client/modules/Loans/Loans.js
@@ -3,13 +3,45 @@ import { connect } from 'react-redux';
 
 // Import Actions
 import { fetchData } from './LoansActions';
+const LineChart = require('react-chartjs').Line;
+
+import styles from './Loans.css';
 
 // Import Selectors
 import { getLoans } from './LoansReducer';
 
+const chartOptions = {
+  scaleBeginAtZero: true,
+  scaleOverride: true,
+  scaleStartValue: 0,
+  scaleSteps: 4,
+  scaleStepWidth: 25,
+};
+
 class LoansPage extends Component {
   componentDidMount() {
     this.props.dispatch(fetchData());
+  }
+
+  getChartInfo(health) {
+    return {
+      labels: health.dates.map((d, i) => i + 1),
+      datasets: [
+        {
+          label: 'Health',
+          fillColor: 'rgba(105,51,147, 0.3)',
+          strokeColor: 'rgba(105,51,147, 0.75)',
+          pointColor: 'rgba(105,51,147, 1)',
+          data: health.values,
+        },
+      ],
+    };
+  }
+
+  getColor(val) {
+    if (val > 80) return 'green';
+    if (val > 50) return 'yellow';
+    return 'red';
   }
 
   render() {
@@ -18,9 +50,19 @@ class LoansPage extends Component {
     if (loans && loans.length) {
       list = loans.map((loan, i) => {
         return (
-          <div key={i}>
-            <h2>{loan.name}</h2>
-            <p>Health: {loan.health}</p>
+          <div key={i} className={styles.loan}>
+            <div className={styles.info}>
+              <h2>{loan.name}</h2>
+              <p>
+                Current Health:
+                <b style={{ color: this.getColor(loan.currentHealth) }}>
+                  &nbsp; {loan.currentHealth}
+                </b>
+              </p>
+            </div>
+            <div className={styles.graph}>
+              <LineChart data={this.getChartInfo(loan.health)} options={chartOptions} width="300" height="100" />
+            </div>
           </div>
         );
       });
--- server/api/controllers/loans.controller.js
+++ server/api/controllers/loans.controller.js
@@ -1,3 +1,5 @@
+import moment from 'moment';
+
 /**
  * Get all Loans
  * @param req
@@ -5,14 +7,80 @@
  * @returns void
  */
 export function getLoans(req, res) {
+  // This is just sample data. Please structure your code in the best way possible. Don't feel obliged to stick to this structure
   return res.send([
     {
-      name: 'Sample loan',
-      health: 88,
+      name: 'Nick Tong\'s Salad Tong Store',
+      currentHealth: 84,
+      health: {
+        dates: [
+          moment('2017/12/01', 'YYYY/MM/DD').toDate(),
+          moment('2017/12/08', 'YYYY/MM/DD').toDate(),
+          moment('2017/12/15', 'YYYY/MM/DD').toDate(),
+          moment('2017/12/22', 'YYYY/MM/DD').toDate(),
+          moment('2017/12/29', 'YYYY/MM/DD').toDate(),
+          moment('2018/01/05', 'YYYY/MM/DD').toDate(),
+          moment('2018/01/12', 'YYYY/MM/DD').toDate(),
+          moment('2018/01/19', 'YYYY/MM/DD').toDate(),
+          moment('2018/01/26', 'YYYY/MM/DD').toDate(),
+          moment('2018/02/02', 'YYYY/MM/DD').toDate(),
+          moment('2018/02/09', 'YYYY/MM/DD').toDate(),
+          moment('2018/02/16', 'YYYY/MM/DD').toDate(),
+          moment('2018/02/13', 'YYYY/MM/DD').toDate(),
+        ],
+        values: [
+          100, // successful payment
+          100,
+          100,
+          100,
+          100,
+          100,
+          100,
+          100,
+          100,
+          90, // decline
+          81, // decline
+          83, // successful payment
+          83, // decline
+          84, // successful payment
+        ],
+      },
     },
     {
-      name: 'Another Loan',
-      health: 20,
+      name: 'Paul\'s Pizzeria',
+      currentHealth: 23,
+      health: {
+        dates: [
+          moment('2017/12/01', 'YYYY/MM/DD').toDate(),
+          moment('2017/12/08', 'YYYY/MM/DD').toDate(),
+          moment('2017/12/15', 'YYYY/MM/DD').toDate(),
+          moment('2017/12/22', 'YYYY/MM/DD').toDate(),
+          moment('2017/12/29', 'YYYY/MM/DD').toDate(),
+          moment('2018/01/05', 'YYYY/MM/DD').toDate(),
+          moment('2018/01/12', 'YYYY/MM/DD').toDate(),
+          moment('2018/01/19', 'YYYY/MM/DD').toDate(),
+          moment('2018/01/26', 'YYYY/MM/DD').toDate(),
+          moment('2018/02/02', 'YYYY/MM/DD').toDate(),
+          moment('2018/02/09', 'YYYY/MM/DD').toDate(),
+          moment('2018/02/16', 'YYYY/MM/DD').toDate(),
+          moment('2018/02/13', 'YYYY/MM/DD').toDate(),
+        ],
+        values: [
+          100, // successful payment
+          50, // decline
+          66, // successful payment
+          50, // decline
+          66, // successful payment
+          50, // decline
+          43, // decline
+          38, // decline
+          33, // decline
+          30, // decline
+          27, // decline
+          25, // decline
+          23, // decline
+        ],
+      },
     },
   ]);
 }
